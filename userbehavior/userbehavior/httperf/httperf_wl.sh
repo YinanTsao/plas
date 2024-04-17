7c714688e3fcfde42b433c13b67839d65f5a77a4
YinanTsao <107555280+YinanTsao@users.noreply.github.com>
2024-04-17 18:03:42 +0200

    Update httperf_wl.sh

--- userbehavior/userbehavior/httperf/httperf_wl.sh
+++ userbehavior/userbehavior/httperf/httperf_wl.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-# Use as many TCP port as possible
-# This test contains 10 sessions, each session has 100 connections, and each connection sends 3 requests
+timestamp=$(date +"%Y%m%d_%H%M%S")
+path="$HOME/userbehavior/httperf/httperf_results/"
+server=130.104.229.12
+port=31112
+uri="/function/tngo"
 
-# httperf \
 
-# --hog \
+# --print-reply --print-request
 
-# --server 130.104.229.36 \
-
-# --port 31112 \
-
-# --uri /function/tngo \
-
-# --wsesslog 2,100,session.txt \
-
-# --period e0.1 \
-
-# --timeout 5
-
-httperf --hog --print-reply --print-request --server 130.104.229.12 --port 31112 --uri /function/tngo --add-header='Content-Type:application/json\n' --wsesslog 200,100,session.txt --period e0.1
+httperf --hog --server $server --port $port --uri $uri --add-header='Content-Type:application/json\n' --wsesslog 500,0,session1.txt --period e0.2 > $path/results_$timestamp.txt
